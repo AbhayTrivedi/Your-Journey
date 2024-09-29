2f98f9220a20a927b76eb2a05ed1f6566d408ba2
Abhay Trivedi <63183752+AbhayTrivedi@users.noreply.github.com>
2024-09-29 23:57:20 +0530

    Add files via upload

---
+++ travelusers.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307
+-- Generation Time: Aug 20, 2023 at 10:04 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `travelusers`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userId` char(24) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `chat1` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`chat1`)),
+  `chat2` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`chat2`)),
+  `chat3` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`chat3`)),
+  `chat4` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`chat4`)),
+  `chat5` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`chat5`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Triggers `users`
+--
+DELIMITER $$
+CREATE TRIGGER `set_default_chat1` BEFORE INSERT ON `users` FOR EACH ROW BEGIN
+	IF NEW.userId IS NULL THEN
+        SET NEW.userId = SUBSTRING(REPLACE(UUID(), '-', ''), 1, 24);
+    END IF;
+    IF NEW.chat1 IS NULL THEN
+        SET NEW.chat1 = '{"chats": [], "welcome": []}';
+	END IF;
+	IF NEW.chat2 IS NULL THEN
+		SET NEW.chat2 = '{"chats": [], "welcome": []}';
+	END IF;
+	IF NEW.chat3 IS NULL THEN
+		SET NEW.chat3 = '{"chats": [], "welcome": []}';
+	END IF;
+	IF NEW.chat4 IS NULL THEN
+		SET NEW.chat4 = '{"chats": [], "welcome": []}';
+	END IF;
+	IF NEW.chat5 IS NULL THEN
+		SET NEW.chat5 = '{"chats": [], "welcome": []}';
+    END IF;
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userId`),
+  ADD UNIQUE KEY `email` (`email`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
